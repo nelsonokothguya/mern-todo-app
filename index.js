6ee53b7aa00b315511c3e4a116aa171833e19fef
root <root@DESKTOP-CBQC8FD.localdomain>
2023-01-17 12:08:29 +0300

    Backend of app

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const bodyParser = require('body-parser'); //interpret client requests to data formats 
+const cors = require('cors'); //specify the sources that are allowed to make requests to my localhost 
+const helmet = require('helmet'); //secure server by adding http headers
+const morgan = require('morgan'); //log req & res (URL, methods, and status codes)
+const {Pool, Client} = require('pg');
+
+
+
+
+
+
+const pool = new Pool({ //create an new instance of Pool (BoundPool) by extending the class Pool from pg
+  host: 'localhost',
+  user: 'postgres',
+  password: 'nelson',
+  database: 'tododb',
+  port: 5432,
+});
+
+
+const app = express();
+const PORT = 443;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/todos', (request, response) => {
+    pool.connect()
+    .then(client => {
+        return client.query('SELECT * FROM todo_list')
+        .then(result => {
+            client.release();
+            response.json(result.rows)
+        })
+        .catch(error => {
+            client.release();
+            console.error(error, "Queries");
+            response.status(500).json({error: 'Error on query'});
+        });
+    })
+    .catch(error => {
+        console.error(error, "DB connection")
+        response.status(500).json({error: "DB connection error"})
+    })
+});
+
+
+
+
+
+
+app.listen(PORT, ()=> {
+    console.log(`Listening to Secure PORT ${PORT}`)
+})
