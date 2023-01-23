8875d131b662b482fa7fa228557e0cbf1ec12fc2
nelsonkimani63 <nelsonkimani63@gmail.com>
2023-01-23 16:22:44 +0300

    routes and mongooseSchema in Express.js

--- index.js
+++ index.js
@@ -1,52 +1,109 @@
-const express = require('express');
-const bodyParser = require('body-parser'); //interpret client requests to data formats 
-
-const {Pool, Client} = require('pg');
-
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
 
 
+//CONNECT TO MONGODB DATABASE
+mongoose.connect('mongodb://localhost/todos', {useNewUrlParser: true})
+mongoose.set('strictQuery', true);
+
+const Schema = mongoose.Schema;
+const TodoSchema = new Schema({
+    text: {
+      type: String,
+      required: true
+    },
+    completed: {
+      type: Boolean,
+      default: false
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  });
+
+const Todo = mongoose.model('Todo', TodoSchema);
 
 
-
-const pool = new Pool({ //create an new instance of Pool (BoundPool) by extending the class Pool from pg
-  host: 'localhost',
-  user: 'postgres',
-  password: 'nelson',
-  database: 'tododb',
-  port: 5432,
-});
-
-
-const app = express();
-const PORT = 443;
+const app = express()
+const PORT = 3000
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 
+// ROUTES
+//ALL TODOS
 app.get('/todos', (request, response) => {
-    pool.connect()
-    .then(client => {
-        return client.query('SELECT * FROM todo_list')
-        .then(result => {
-            client.release();
-            response.json(result.rows)
-        })
-        .catch(error => {
-            client.release();
-            console.error(error, "Queries");
-            response.status(500).json({error: 'Error on query'});
-        });
+    //find all todos in the database
+
+    Todo.find({}, (error, todos) => {
+        if (error) {
+            response.status(500).send(error)
+        } else {
+            response.send(todos)
+        }
     })
-    .catch(error => {
-        console.error(error, "DB connection")
-        response.status(500).json({error: "DB connection error"})
+    
+});
+
+// CREATE TODOS
+
+app.post('/todos', (request, response) => {
+    //create todo
+
+    const todo = new Todo({text: request.body.text});
+
+    //save the created todo to the DB
+    todo.save((error, todo)=> {
+        if (error) {
+            response.status(500).send(error);
+        } else {
+            response.send(todo);
+        }
+    });
+});
+
+
+//EDIT TODOS
+
+app.put('/todos/:id', (request, response)=> {
+    Todo.findById(request.params.id, (error, todo) => {
+        if (error) {
+            response.status(500).send(error);
+        } else {
+            todo.text = request.body.text;
+            todo.save((error, todo) => {
+                if (error) {
+                    response.status(500).send(error);
+                } else {
+                    response.send(todo)
+                }
+            })
+        }
     })
 });
 
 
 
+// REMOVE TODOS
+
+app.delete('/todos/:id', (request, response) => {
+    Todo.findByIdAndRemove(request.params.id, (error, todo)=> {
+        if(error) {
+            response.status(500).send(error);
+        } else {
+            response.send(todo)
+        }
+    })
+
+})
 
 
+
+
+
+// LISTENING FUNCTION
 
 app.listen(PORT, ()=> {
     console.log(`Listening to Secure PORT ${PORT}`)
---
+++ test/todo.routes.test.js
@@ -0,0 +1,4 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const app = require('../index');
+const Todo = require('../models/Todo');
