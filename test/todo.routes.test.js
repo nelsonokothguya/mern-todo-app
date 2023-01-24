f5200ea96f0a3c0a00b38b9858b1b9d9bac00765
nelsonkimani63 <nelsonkimani63@gmail.com>
2023-01-24 12:35:57 +0300

    updates to test file using mocha test suite

--- test/todo.routes.test.js
+++ test/todo.routes.test.js
@@ -1,48 +1,48 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const app = require('../index'); // or the path to your index.js file
+const app = require('../index');
+const { Todo } = require('../index');
 
 chai.use(chaiHttp);
 
 describe('GET /', () => {
     it('should return all todos', async () => {
-      const res = await chai.request(app).get('/');
-      expect(res).to.have.status(200);
-      expect(res.body).to.be.an('array');
-      // you can assert other properties of the returned todos here
+        const res = await chai.request(app).get('/');
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('array');
     });
 });
 
 describe('POST /todos', () => {
     it('should create a new todo', async () => {
-      const res = await chai.request(app)
-        .post('/todos')
-        .send({ text: 'Test todo' });
-      expect(res).to.have.status(201);
-      expect(res.body).to.be.an('object');
-      expect(res.body).to.have.property('text', 'Test todo');
-      // you can assert other properties of the created todo here
+        const res = await chai.request(app)
+            .post('/todos')
+            .send({ text: 'Test todo' });
+        expect(res).to.have.status(201);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('text', 'Test todo');
     });
 });
 
 describe('PUT /todos/:id', () => {
     it('should update a todo', async () => {
-      const res = await chai.request(app)
-        .put('/todos/5c9a8e0fcfa7a919f4c6e7e8')
-        .send({ completed: true });
-      expect(res).to.have.status(200);
-      expect(res.body).to.be.an('object');
-      expect(res.body).to.have.property('completed', true);
-      // you can assert other properties of the updated todo here
+        const todo = await Todo.create({ text: 'Test todo' });
+        const res = await chai.request(app)
+            .put(`/todos/${todo._id}`)
+            .send({ completed: true });
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('completed', true);
     });
 });
 
 describe('DELETE /todos/:id', () => {
     it('should delete a todo', async () => {
-      const res = await chai.request(app)
-        .delete('/todos/5c9a8e0fcfa7a919f4c6e7e8');
-      expect(res).to.have.status(200);
-      expect(res.body).to.be.an('object');
-      expect(res.body).to.have.property('message', 'Todo deleted successfully');
+        const todo = await Todo.create({ text: 'Test todo' });
+        const res = await chai.request(app)
+            .delete(`/todos/${todo._id}`);
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('message', 'Todo deleted successfully');
     });
 });
